130e84ac71cd5eef5666101e6481055f72a670d6
S010MON <leon_debnath@hotmail.co.uk>
2023-02-28 21:07:10 +0100

    refactor extraction script

--- DataExtraction.py
+++ DataExtraction.py
@@ -1,127 +1,44 @@
-from serpapi import GoogleSearch
 import pandas as pd
 import os
 
 
-class DataExtraction:
-    def __init__(self, location):
-        self.location = location
-        self.qualifications = []
-        self.benefits = []
-        self.highlights = []
-        self.responsibilities = []
+def merge_highlights(highlights: list) -> list:
+    """
+    A function that takes a list of dictionaries and strings and merges
+    them into a list of individual strings by extracting the values with
+    'item' tags and ignoring 'title'
+    """
+    output = []
+    for highlight in highlights:
 
-    def extract(self):
-        raw_data = self.api_call()
+        if type(highlight) is dict:
+            output.extend(highlight['items'])
 
-        df = self.df_generation(raw_data)
-
-        df = self.data_cleaning(df)
-
-        self.highlights_extraction(df)
-
-        df_final = self.column_matching(df)
-
-        self.write_data(df_final)
-
-    # API Key Store
-    # Travis: 7b49b198668e629ad2ea004850f238d18e56e6e6f3ed19a4d71430a670f4ba29
-    # Leon: a50a83b24d1e51c42d6567fb1bd517dadd4d4348957fe3450928060bbb400ada
-    # Konni:
-    def api_call(self):
-        params = {
-            "engine": "google_jobs",
-            "q": "data scientist",
-            "hl": "en",
-            "api_key": "a50a83b24d1e51c42d6567fb1bd517dadd4d4348957fe3450928060bbb400ada",
-            "gl": "uk",
-            "location": self.location
-        }
-
-        search = GoogleSearch(params)
-        results = search.get_dict()
-        return results["jobs_results"]
-
-    def df_generation(self, scraped_data):
-        # Generate raw dataframe from scraped job listings
-        df = pd.DataFrame.from_records(scraped_data)
-        return df
-
-    def data_cleaning(self, df):
-        # Remove unnecessary bracket in Location
-        df['location'] = df['location'].str.replace('\([^()]*\)', "", regex=True)
-
-        # Remove via keyword from via
-        df['via'] = df['via'].str.replace('via', '', regex=False)
-
-        # Remove new line and other unnecessary characters
-        df = df.replace(to_replace=r'\n\n|\n•|\n', value=' ', regex=True)
-
-        # Remove unncecessary columns from the dataframe
-        df.drop(columns=['thumbnail', 'extensions', 'related_links'], inplace=True)
-        return df
-
-    def highlights_extraction(self, df):
-        df['job_highlights'].apply(self.extract_inner_df)
-        self.merging_highlights(df)
-
-    def highlights_selection(self, title, items):
-        match title:
-            case 'Qualifications':
-                self.qualifications.append(items)
-            case 'Responsibilities':
-                self.responsibilities.append(items)
-            case 'Benefits':
-                self.benefits.append(items)
-            case None:
-                self.highlights.append(items)
-
-    def extract_inner_df(self, x):
-        for highlight in x:
-            column_title = highlight.get('title')
-            column_items = highlight.get('items')
-            self.highlights_selection(column_title, column_items)
-        self.equal_series()
-
-    def equal_series(self):
-        lengths = [len(self.qualifications), len(self.responsibilities), len(self.benefits), len(self.highlights)]
-        max_length = max(lengths)
-
-        if len(self.qualifications) < max_length:
-            self.qualifications.append(None)
-        if len(self.responsibilities) < max_length:
-            self.responsibilities.append(None)
-        if len(self.benefits) < max_length:
-            self.benefits.append(None)
-        if len(self.highlights) < max_length:
-            self.highlights.append(None)
-
-    def merging_highlights(self, df):
-        df.drop(columns=['job_highlights'], inplace=True)
-        df['qualifications'] = self.qualifications
-        df['responsibilities'] = self.responsibilities
-        df['benefits'] = self.benefits
-        df['highlights'] = self.highlights
-
-    def column_matching(self, df):
-        try:
-            df = df[['title', 'company_name', 'location', 'via', 'description',
-                         'detected_extensions', 'highlights', 'responsibilities', 'qualifications',
-                         'benefits', 'job_id']]
-            return df
-        except:
-            print("!Essential columns missing!")
-
-    def write_data(self, df):
-        file_name = 'interim_data.csv'
-        data_exists = os.path.isfile(file_name)
-        if data_exists:
-            df.to_csv(file_name, mode='a', index=False, header=False)
-        else:
-            df.to_csv(file_name, mode='w', index=False, header=True)
+        elif type(highlight) is str:
+            output.append(highlight)
+    return output
 
 
-if __name__ == "__main__":
-    data_pipeline = DataExtraction("Netherlands")
-    data_pipeline.extract()
+def write_data(df: pd.DataFrame) -> None:
+    file_name = 'data.csv'
+    data_exists = os.path.isfile(file_name)
+    if data_exists:
+        df.to_csv(file_name, mode='a', index=False, header=False)
+    else:
+        df.to_csv(file_name, mode='w', index=False, header=True)
+
+
+def extract(json_response: dict) -> None:
+    """
+    Insert SerpAPI Google Jobs response directly in to extract
+    data will be appended to 'data.csv' in the root folder.
+    """
+    df = pd.DataFrame.from_records(json_response["jobs_results"])
+    df['location'] = df['location'].str.replace('\([^()]*\)', "", regex=True)   # Remove unnecessary bracket in Location
+    df['via'] = df['via'].str.replace('via', '', regex=False)                   # Remove via keyword from vi
+    df = df.replace(to_replace=r'\n\n|\n•|\n', value=' ', regex=True)           # Remove new line and other characters
+    df.drop(columns=['thumbnail', 'extensions', 'related_links'],inplace=True)  # Remove columns from the dataframe
+    df.job_highlights = df.job_highlights.apply(merge_highlights)               # Place dict/list of strings into a single list
+
+    write_data(df)
 
